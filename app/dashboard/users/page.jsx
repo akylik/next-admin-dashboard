d4e29cbb0920cd5cb19f09b42808339ef6848e63
o.kulyk <o.kulyk@ejaw.net>
2024-02-21 01:03:58 +0200

    add pagination

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -5,16 +5,17 @@ import Image from "next/image";
 import Pagination from "@/app/ui/dashboard/pagination/pagination";
 import { fetchUsers } from "@/app/lib/data";
 
+const UsersPage = async ({ searchParams }) => {
+  const q = searchParams?.q || "";
+  const page = searchParams?.page || 1;
 
-const UsersPage = async() => {
-  const users = await fetchUsers();
-  console.log('users >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>',users);
-  
+  const { users, count } = await fetchUsers(q, page);
+
   return (
     <div className={styles.container}>
       <div className={styles.top}>
-        <Search placeholder="Search for a user..."/>
-        < Link href="/dashboard/users/add">
+        <Search placeholder="Search for a user..." />
+        <Link href="/dashboard/users/add">
           <button className={styles.addButton}>Add New</button>
         </Link>
       </div>
@@ -30,39 +31,47 @@ const UsersPage = async() => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <div className={styles.user}>
-                <Image
-                  className={styles.userImage}
-                  src="/noavatar.png"
-                  alt="user"
-                  width={40}
-                  height={40}
-                />
-                John Doe
-              </div>
-            </td>
-            <td>5H8wS@example.com</td>
-            <td>13.01.2022</td>
-            <td>Admin</td>
-            <td>
-              <span className={`${styles.status} ${styles.active}`}>active</span>
-            </td>
-            <td>
-              <div className={styles.buttons}>
-                <Link href="/dashboard/users/test">
-                  <button className={`${styles.button} ${styles.view}`}>View</button>
-                </Link>
-                <button className={`${styles.button} ${styles.delete}`}>Delete</button>
-              </div>
-            </td>
-          </tr>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>
+                <div className={styles.user}>
+                  <Image
+                    className={styles.userImage}
+                    src={user.img || "/noavatar.png"}
+                    alt="user"
+                    width={40}
+                    height={40}
+                  />
+                  {user.username}
+                </div>
+              </td>
+              <td>{user.email}</td>
+              <td>{user.createdAt?.toString().slice(4, 16)}</td>
+              <td>{user.isAdmin ? "Admin" : "Client"}</td>
+              <td>
+                <span className={`${styles.status} ${styles.active}`}>
+                  {user.isActive ? "active" : "passive"}
+                </span>
+              </td>
+              <td>
+                <div className={styles.buttons}>
+                  <Link href={`/dashboard/users/${user.id}`}>
+                    <button className={`${styles.button} ${styles.view}`}>
+                      View
+                    </button>
+                  </Link>
+                  <button className={`${styles.button} ${styles.delete}`}>
+                    Delete
+                  </button>
+                </div>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
-      <Pagination/>
+      <Pagination count={count} />
     </div>
-  )
-}
+  );
+};
 
-export default UsersPage
+export default UsersPage;
--- app/lib/data.js
+++ app/lib/data.js
@@ -1,11 +1,15 @@
+import { ITEM_PER_PAGE } from "../config";
 import { User } from "./models";
 import { connectToDB } from "./utils";
 
-export const fetchUsers = async () => {
+export const fetchUsers = async (q, page) => {
+  const regex = new RegExp(q, "i");
+
   try {
     connectToDB();
-    const users = await User.find();
-    return users;
+    const count = await User.find({username: {$regex: regex}}).count();
+    const users = await User.find({username: {$regex: regex}}).limit(ITEM_PER_PAGE).skip(ITEM_PER_PAGE * (page - 1));
+    return {users, count};
   } catch (error) {
     console.log('Error fetching users:', error);
     throw new Error('Error fetching users');
